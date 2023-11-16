2e76ccbce437b3f0f0cc1563bdda4062ff0e4c36
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-11-16 20:58:09 -0300

    refactor(backend): simplify getRandomInteger

--- backend/src/utils/getRandomInteger.ts
+++ backend/src/utils/getRandomInteger.ts
@@ -1,27 +1,9 @@
 /**
- * Draws a random integer between 1 and the given value
- * @param max The maximum value
- * @returns The random integer
- */
-export function getRandomInteger(max: number): number
-
-/**
  * Draws a random integer between the requested range
  * @param min The minimum value
  * @param max The maximum value
  * @returns The random integer
  */
-export function getRandomInteger(min: number, max: number): number
-
-export function getRandomInteger(firstValue: number, secondValue?: number): number {
-  const isSecondValueDefined = typeof secondValue === "number"
-
-  const min = isSecondValueDefined ? firstValue : 1;
-  const max = isSecondValueDefined ? secondValue : firstValue;
-
-  const randomInteger = Math.floor(
-    Math.random() * (max - min + 1) + min
-  );
-
-  return randomInteger;
-}
+export const getRandomInteger = (min: number, max: number) => Math.floor(
+  Math.random() * (max - min + 1) + min
+);
