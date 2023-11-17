d75e72b992d63ac80b404baade45769f00fbfede
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-11-16 21:17:36 -0300

    feat(frontend): render maze

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,32 @@
 import './style.css'
+
+import { io, Socket } from "socket.io-client"
+
+import { ServerToClientEvents } from "./domain"
+
+const canvas = document.querySelector<HTMLCanvasElement>("#game")!
+const ctx = canvas.getContext("2d")!
+
+const gameProportionInPixels = 10
+
+const socket: Socket<ServerToClientEvents> = io("http://localhost:3000")
+
+socket.on("maze", ({ dimensions, mazePath, maze }) => {
+  canvas.width = (2 * dimensions.width - 1) * gameProportionInPixels
+  canvas.height = (2 * dimensions.height - 1) * gameProportionInPixels
+  canvas.style.border = `solid ${gameProportionInPixels}px black`
+
+  console.log(mazePath)
+
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      if (maze[i][j] === 1)
+        ctx.fillRect(
+          j * gameProportionInPixels,
+          i * gameProportionInPixels,
+          gameProportionInPixels,
+          gameProportionInPixels
+        )
+    }
+  }
+})
