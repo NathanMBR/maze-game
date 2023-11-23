fbecf120bb7c2daedd263b766da745636776a7c1
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-11-22 21:53:15 -0300

    feat(backend): add movement event

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
 
 import {
+  Player,
   createMatrix,
   getMazePath,
-  getPixelRepresentation
+  getPixelRepresentation,
+  ClientToServerEvents,
+  ServerToClientEvents,
+  Coordinates
 } from "./domain"
+import { getRandomInteger, isMazeTileOccupied } from './utils';
 import { PORT } from "./settings"
 
 const mazeWidth = 30
@@ -13,20 +18,77 @@ const mazeHeight = 30
 const mazePath = getMazePath(createMatrix(mazeWidth, mazeHeight))
 const maze = getPixelRepresentation(mazePath)
 
-const io = new Server(PORT, {
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(PORT, {
   cors: {
     origin: "*"
   }
 })
 
+const players = new Map<string, Player>()
+
 io.on("connection", socket => {
-  socket.emit("maze", {
-    mazePath,
-    maze,
-    dimensions: {
-      width: mazeWidth,
-      height: mazeHeight
+  const playerId = socket.id
+
+  const emptyMazeTiles = maze.map((row, y) => {
+    return row.map((tile, x) => {
+      if (tile === 0)
+        return { x, y }
+
+      return null
+    }).filter(tile => !!tile) as Array<{ x: number, y: number }>
+  }).reduce((list, row) => {
+    list.push(...row)
+    return list
+  }, [])
+
+  const randomStartPositionIndex = getRandomInteger(0, emptyMazeTiles.length - 1)
+  const randomStartPosition = emptyMazeTiles[randomStartPositionIndex]
+
+  players.set(socket.id, {
+    id: playerId,
+    position: randomStartPosition
+  })
+
+  const sendState = () => {
+    io.emit("maze", {
+      mazePath,
+      maze,
+      dimensions: {
+        width: mazeWidth,
+        height: mazeHeight
+      },
+      players: Array.from(players.values())
+    })
+  }
+
+  sendState()
+
+  socket.on("movement", direction => {
+    const player = players.get(playerId)
+    if (!player)
+      return
+
+    const position: Coordinates = {
+      x: direction === "RIGHT" ? player.position.x + 1 : direction === "LEFT" ? player.position.x - 1 : player.position.x,
+      y: direction === "DOWN" ? player.position.y + 1 : direction === "UP" ? player.position.y - 1 : player.position.y
     }
+
+    const isDirectionAvailable = isMazeTileOccupied(maze, position.x, position.y)
+    if (!isDirectionAvailable)
+      return
+
+    players.set(playerId, {
+      ...player,
+      position
+    })
+
+    sendState()
+  })
+
+  socket.on("disconnect", () => {
+    players.delete(socket.id)
+
+    sendState()
   })
 })
 
