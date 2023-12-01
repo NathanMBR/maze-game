fe56730c464928cfca5668a8baadfac870889250
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-12-01 13:49:03 -0300

    feat(frontend): add Bullet

---
+++ frontend/src/domain/types/Bullet.ts
@@ -0,0 +1,6 @@
+import { Coordinates } from "./Coordinates"
+
+export type Bullet = {
+  id: string
+  position: Coordinates
+}
--- frontend/src/domain/types/index.ts
+++ frontend/src/domain/types/index.ts
@@ -2,6 +2,4 @@ export * from "./Maze"
 export * from "./MazeEventData"
 export * from "./Coordinates"
 export * from "./Direction"
-
-export * from "./ServerToClientEvents"
-export * from "./ClientToServerEvents"
+export * from "./Bullet"
