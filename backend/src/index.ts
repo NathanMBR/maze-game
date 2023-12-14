5779d41673d4b6fc66006f913923f938ff63774a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-12-13 23:25:29 -0300

    feat(backend): make bombs spawn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import {
   type ServerToClientEvents,
   type Coordinates,
   type Bullet,
+  type Bomb,
   createMatrix,
   getMazePath,
   getPixelRepresentation,
@@ -25,7 +26,8 @@ const mazeWidth = 20
 const mazeHeight = 20
 const shootDelayInMilliseconds = 500
 const explosionRadiusInTiles = 5
-const initialBombsCount = 3
+const initialBombsCount = 1
+const bombSpawnDelayInMilliseconds = 10 * 1000
 
 const mazePath = getMazePath(createMatrix(mazeWidth, mazeHeight))
 const maze = getPixelRepresentation(mazePath)
@@ -39,28 +41,80 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
 
 const players = new Map<string, Player>()
 const bullets = new Map<string, Bullet>()
+const bombs = new Map<string, Bomb>()
+
+const getEmptyMazeTiles = () => maze.map((row, y) => {
+  return row.map((tile, x) => {
+    const isPathTile = tile === 0
+    const hasPlayerInCurrentCoordinates = Array.from(players.values()).some(({ position }) => position.x === x && position.y === y)
+    const hasBombInCurrentCoordinates = Array.from(bombs.values()).some(({ position }) => position.x === x && position.y === y)
+
+    const isTileEmpty =
+      isPathTile &&
+      !hasPlayerInCurrentCoordinates &&
+      !hasBombInCurrentCoordinates
+
+    if (!isTileEmpty)
+      return null
+
+    return { x, y }
+  }).filter(tile => !!tile) as Array<{ x: number, y: number }>
+}).reduce((list, row) => {
+  list.push(...row)
+  return list
+}, [])
+
+const sendState = () => {
+  io.emit("maze", {
+    mazePath,
+    maze,
+    dimensions: {
+      width: mazeWidth,
+      height: mazeHeight
+    },
+    players: Array.from(players.values()),
+    bullets: Array.from(bullets.values()),
+    bombs: Array.from(bombs.values())
+  })
+}
+
+const killPlayer = (id: string) => {
+  players.delete(id)
+  io.to(id).emit("death")
+}
+
+const spawnBomb = () => {
+  setTimeout(spawnBomb, bombSpawnDelayInMilliseconds)
+
+  const minimumPlayersQuantityToSpawnBomb = 2
+  const maximumSpawnedBombs = 5
+
+  if (players.size < minimumPlayersQuantityToSpawnBomb)
+    return
+
+  if (bombs.size >= maximumSpawnedBombs)
+    return
+
+  const emptyMazeTiles = getEmptyMazeTiles()
+
+  const randomSpawnPositionIndex = getRandomInteger(0, emptyMazeTiles.length - 1)
+  const randomSpawnPosition = emptyMazeTiles[randomSpawnPositionIndex]
+
+  const bombId = randomUUID()
+  bombs.set(bombId, {
+    id: bombId,
+    position: randomSpawnPosition
+  })
+
+  sendState()
+}
+
+setTimeout(spawnBomb, bombSpawnDelayInMilliseconds)
 
 io.on("connection", socket => {
   const playerId = socket.id
 
-  const emptyMazeTiles = maze.map((row, y) => {
-    return row.map((tile, x) => {
-      const isPathTile = tile === 0
-      const hasPlayerInCurrentCoordinates = Array.from(players.values()).some(({ position }) => position.x === x && position.y === y)
-
-      const isTileEmpty =
-        isPathTile &&
-        !hasPlayerInCurrentCoordinates
-
-      if (!isTileEmpty)
-        return null
-
-      return { x, y }
-    }).filter(tile => !!tile) as Array<{ x: number, y: number }>
-  }).reduce((list, row) => {
-    list.push(...row)
-    return list
-  }, [])
+  const emptyMazeTiles = getEmptyMazeTiles()
 
   const randomStartPositionIndex = getRandomInteger(0, emptyMazeTiles.length - 1)
   const randomStartPosition = emptyMazeTiles[randomStartPositionIndex]
@@ -74,44 +128,32 @@ io.on("connection", socket => {
       bombs: initialBombsCount
     })
 
-  const sendState = () => {
-    io.emit("maze", {
-      mazePath,
-      maze,
-      dimensions: {
-        width: mazeWidth,
-        height: mazeHeight
-      },
-      players: Array.from(players.values()),
-      bullets: Array.from(bullets.values())
-    })
-  }
-
   sendState()
-
-  const killPlayer = (id: string) => {
-    players.delete(id)
-    socket.to(id).emit("death")
-  }
 
   socket.on("walk", direction => {
     const player = players.get(playerId)
     if (!player)
       return
 
-    const position: Coordinates = {
+    const newPlayerPosition: Coordinates = {
       x: direction === "RIGHT" ? player.position.x + 1 : direction === "LEFT" ? player.position.x - 1 : player.position.x,
       y: direction === "DOWN" ? player.position.y + 1 : direction === "UP" ? player.position.y - 1 : player.position.y
     }
 
-    const isDirectionAvailable = isMazeTileAvailable(maze, position.x, position.y)
+    const isDirectionAvailable = isMazeTileAvailable(maze, newPlayerPosition.x, newPlayerPosition.y)
     if (!isDirectionAvailable)
       return
 
+    const bombBelowPlayer = Array.from(bombs.values()).find(bomb => bomb.position.x === newPlayerPosition.x && bomb.position.y === newPlayerPosition.y)
+
     players.set(playerId, {
       ...player,
-      position
+      position: newPlayerPosition,
+      bombs: bombBelowPlayer ? player.bombs + 1 : player.bombs
     })
+
+    if (bombBelowPlayer)
+      bombs.delete(bombBelowPlayer.id)
 
     sendState()
   })
